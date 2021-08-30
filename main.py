09c3e723c94080488af29ad8e345acc3f4593f1f
TUVYAGEL <65275611+TUVYAGEL@users.noreply.github.com>
2021-08-30 13:02:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,65 @@ if __name__ == '__main__':
         'creation_date': '1628406616705',
 
         #details
-            #'severity': 'low',
-            #'status': 'error',
-            #'tags':json.dumps([{"name": 'application_bucket'}, {"size": '10GB'}, {"region": 'us_west_2'}])
+           'severity': 'high',
+            'status': 'error',
+            'tags':json.dumps([{"name": 'application_bucket'}, {"size": '10GB'}, {"region": 'us_west_2'}])
         })
 
-    # extracting response text
-    pastebin_url = r.text
-    print("The pastebin URL is:%s" % pastebin_url)
+        # extracting response text
+        pastebin_url = r.text
+        print("The pastebin URL is:%s" % pastebin_url)
+
+
+
+	r = requests.post(url=API_ENDPOINT, params={
+        'resource': 'aws_api_gateway_api_key',
+        'category': 'Logging',
+        'creation_date': '1628406616705',
+
+        #details
+            'severity': 'low',
+            'status': 'passed',
+            'tags':json.dumps([{"name": 'application_bucket'}, {"size": '20GB'}, {"region": 'us_west_2'}])
+        })
+
+        # extracting response text
+        pastebin_url = r.text
+        print("The pastebin URL is:%s" % pastebin_url)
+
+
+
+
+
+    r = requests.post(url=API_ENDPOINT, params={
+            'resource': 'aws_api_gateway_api_key',
+            'category': 'Logging',
+            'creation_date': '1638406236725'
+            })
+
+        # extracting response text
+        pastebin_url = r.text
+        print("The pastebin URL is:%s" % pastebin_url)
+
+
+
+
+
+
+
+	r = requests.post(url=API_ENDPOINT, params={
+        'resource': 'aws_api_gateway_api_key',
+        'category': Networking
+        'creation_date': '1628406616705',
+
+        #details
+            'severity': 'low',
+            'status': 'passed'
+        })
+
+        # extracting response text
+        pastebin_url = r.text
+        print("The pastebin URL is:%s" % pastebin_url)
+
+
 
